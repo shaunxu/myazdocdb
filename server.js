5bed6734e640c4feb0cbd38b029af4468e424774
Shaun Xu <jfarrio@gmail.com>
2014-08-25 16:29:58 +0800

    web-server.js for angular

---
+++ server.js
@@ -0,0 +1,58 @@
+(function () {
+    'use strict';
+
+    var path = require('path');
+    var argv = require('optimist').usage('Usage: $0 [--port] [--debug]')
+        .default('port', process.env.port || 80)
+        .default('debug', true)
+        .argv;
+
+    var log4js = require('log4js');
+    var logger = log4js.getLogger('WEB-SERVER');
+    if (argv['debug'] === true) {
+        logger.setLevel('DEBUG');
+    }
+    else {
+        logger.setLevel('INFO');
+    }
+
+    logger.debug('config - port: ' + argv['port']);
+    logger.debug('config - debug: ' + argv['debug']);
+
+    var express = require('express');
+    var app = express();
+
+    // log
+    app.use(function (req, res, next) {
+        var requestTimeInMs = Date.now();
+        res.on('finish', function () {
+            var durationInMs = Date.now() - requestTimeInMs;
+            logger.debug('[' + req.method + '] ' + durationInMs + 'ms: ' + req.url);
+        });
+        next();
+    });
+    // url encoding
+    var bodyParser = require('body-parser')
+    app.use(bodyParser.urlencoded());
+    // gzip
+    var compression = require('compression')
+    app.use(compression());
+    // redirect all html requests to `index.html`
+    app.use(function (req, res, next) {
+        if (path.extname(req.path).length > 0) {
+            // normal static file request
+            next();
+        }
+        else {
+            // should force return `index.html` for angular.js
+            req.url = '/index.html';
+            next();
+        }
+    });
+    // static file serve
+    app.use(express.static(__dirname));
+
+    app.listen(argv['port']);
+    logger.info('http://0.0.0.0:' + argv['port'] + '/');
+    process.title = 'My Azure DocumentDB';
+})();
