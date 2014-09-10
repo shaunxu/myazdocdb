82f4b2fec02fe53bdc3f3728852a343687e8dc52
Shaun Xu <jfarrio@gmail.com>
2014-09-10 10:47:00 +0800

    still struggling proxy, no luck

--- request-proxy.js
+++ request-proxy.js
@@ -4,7 +4,6 @@
     var http = require('http');
     var https = require('https');
     var __httpRequest = http.request;
-    var __httpsRequest = https.request;
 
     var proxy = {
         host: '',
@@ -23,26 +22,49 @@
             _logger.debug(JSON.stringify(__options, null, 2));
             _logger.debug('=== request-proxy.js [http] end debug ===');
         }
-        var req = __httpRequest(__options, function (res) {
-            callback(res);
-        });
+        var req = __httpRequest.apply(http, [__options, function (res) {
+            return callback(res);
+        }]);
         return req;
     };
 
     https.request = function (options, callback) {
-        var __options = options;
-        __options.path = 'https://' + options.host + options.path;
-        __options.host = proxy.host;
-        __options.port = proxy.port;
+        var __options = {
+            protocol: 'http:',
+            host: proxy.host,
+            port: proxy.port,
+            method: options.method,
+            path: options.protocol + '//' + options.host + options.path,
+            headers: options.headers
+        };
+        __options.headers['host'] = options.host;
         if (_logger) {
             _logger.debug('=== request-proxy.js [https] begin debug ===');
+            _logger.debug('=== original options');
+            _logger.debug(JSON.stringify(options, null, 2));
+            _logger.debug('=== actual options');
             _logger.debug(JSON.stringify(__options, null, 2));
             _logger.debug('=== request-proxy.js [https] end debug ===');
         }
-        var req = __httpsRequest(__options, function (res) {
-            callback(res);
-        });
+
+        var req = __httpRequest.apply(http, [__options, function (res) {
+            return callback(res);
+        }]);
         return req;
+
+        //var __options = options;
+        //__options.path = 'https://' + options.host + options.path;
+        //__options.host = proxy.host;
+        //__options.port = proxy.port;
+        //if (_logger) {
+        //    _logger.debug('=== request-proxy.js [https] begin debug ===');
+        //    _logger.debug(JSON.stringify(__options, null, 2));
+        //    _logger.debug('=== request-proxy.js [https] end debug ===');
+        //}
+        //var req = __httpsRequest.apply(https, [__options, function (res) {
+        //    callback(res);
+        //}]);
+        //return req;
     };
 
     module.exports = function (host, port, logger) {
